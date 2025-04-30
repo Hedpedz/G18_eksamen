d4b2c1d3c9710c7ce07d0766a4239d90b4e7b385
Hedpedz <heddaon@hiof.no>
2025-04-30 16:17:25 +0200

    Implemented routing and add initial page components for Home, EventPage, CategoryPage, and Dashboard

--- billettlyst/src/App.jsx
+++ billettlyst/src/App.jsx
@@ -1,1 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import EventPage from './pages/EventPage';
+import CategoryPage from './pages/CategoryPage';
+import Dashboard from './pages/Dashboard';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event/:id" element={<EventPage />} />
+          <Route path="/category/:slug" element={<CategoryPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Layout>
+      {/* Attribution link til APIet */}
+    </BrowserRouter>
+  );
+}
+export default App;
