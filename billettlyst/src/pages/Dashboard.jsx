95f88320fa4e5797461f31b6011c21a417b27517
Markus Olsen <markusol@hiof.no>
2025-05-08 14:23:48 +0200

    Added simple login-form which console logs the input from the form.

--- billettlyst/src/pages/Dashboard.jsx
+++ billettlyst/src/pages/Dashboard.jsx
@@ -1,5 +1,31 @@
-export default function Dashboard() {
+import React, { useState } from "react";
 
-  return <h1>Hello World</h1>
+export default function Dashboard() {
+  const [username, setUsername] = useState("");
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(username);
+  }
+
+  return(
+    <section>
+      
+    <h1>Logg inn</h1>
+    <form>
+      <label>
+        Brukernavn
+        <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        >
+        </input>
+      </label>
+      <button onClick={handleClick}>Logg inn</button>
+    </form> 
+
+    </section>
+  ) 
  
 }
