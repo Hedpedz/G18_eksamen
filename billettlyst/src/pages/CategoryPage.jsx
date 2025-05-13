e5c80ddfe44649ee7da99a59a7323bbe71ce9fc8
Arshad Abba <arshadshekel@gmail.com>
2025-05-13 20:35:15 +0200

    Temporary backup of what I've done

--- billettlyst/src/pages/CategoryPage.jsx
+++ billettlyst/src/pages/CategoryPage.jsx
@@ -1,7 +1,62 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import EventCard from "../components/EventCard";
+import '../styles/CategoryPage.scss';
+
 export default function CategoryPage() {
   const { slug } = useParams();
+  const [category, setCategory] = useState(null);
+  const [events, setEvents] = useState([]);
 
-  return  <h1> kategori: {slug}</h1>
+  useEffect(() => {
+    if (slug === "teater") {
+      setCategory("Theatre");
+    } else if (slug === "sport") {
+      setCategory("Sport");
+    } else if (slug === "musikk") {
+      setCategory("Music");
+    } else {
+      setCategory(null);
+    }
+  }, [slug]);
 
+  useEffect(() => {
+    if (category) {
+      async function fetchEvents() {
+        try {
+          const response = await fetch(
+            `https://app.ticketmaster.com/discovery/v2/events?apikey=An0Gfh3JYmKpW5rJIqCetXQuRadlfUhp&countryCode=NO&classificationName=${category}&size=10`
+          );
+          const json = await response.json();
+          setEvents(json);
+        } catch (error) {
+          console.error("Fetch error:", error);
+        }
+      }
+
+      fetchEvents();
+    }
+  }, [category]);
+
+  return (
+    <section
+      className="EventContainer"
+    >
+      {events._embedded?.events ? (
+        events._embedded.events.map((event) => (
+
+          <EventCard
+            key={event.id}
+            name={event.name}
+            date={event.dates.start.localDate}
+            image={event.images[0]?.url}
+            link={event.id}
+          />
+
+        ))
+      ) : (
+        <p>No events found.</p>
+      )}
+    </section>
+  );
 }
--- billettlyst/src/pages/Home.jsx
+++ billettlyst/src/pages/Home.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
 export default function Home() {
-  
-  
-      
+
+    const [events, setEvents] = useState([]);
+    const [city, setCity] = useState(null);
+
+    useEffect(() => {
+        async function fetchEvents() {
+            try {
+              const response = await fetch(
+                `https://app.ticketmaster.com/discovery/v2/events?apikey=An0Gfh3JYmKpW5rJIqCetXQuRadlfUhp&city=Paris&size=10`
+              );
+              const json = await response.json();
+              setEvents(json);
+              console.log(events);
+            } catch (error) {
+              console.error("Fetch error:", error);
+            }
+          }
+    
+          fetchEvents();
+      }, []);
+
+    return (
+    <>
+        {events ? (
+        
+        <pre>{JSON.stringify(events, null, 2)}</pre>
+      ) : (
+        <p>Loading event...</p>
+      )}
+    </>  
+    )
 }
