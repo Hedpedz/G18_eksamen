60648739f47cc3ac5495e7a730f135080ad7d452
Markus Olsen <markusol@hiof.no>
2025-05-19 05:38:05 +0200

    Added some aria-labels

--- billettlyst/src/components/AttractionCard.jsx
+++ billettlyst/src/components/AttractionCard.jsx
@@ -7,13 +7,21 @@ export default function AttractionCard({ id, name, image, showHeart, isSaved, on
   const heartIcon = isSaved ? solidHeartIcon : regularHeartIcon;
 
   return (
-    <article className="event-card">
+    <article className="event-card"
+    aria-label={`Attraksjon: ${name}`}
+    >
     <img src={image} alt={name}/>
       <h3>{name}</h3>
       {showHeart &&(
         <button
         onClick={() => onToggleSave(id)}
         className= "heart-button"
+         aria-label={
+          isSaved 
+          ? `Fjern ${name} fra ønskelisten`
+          : `Legg til ${name} i ønskelisten`
+        }
+        
         >
           <img src={heartIcon} 
           alt="Legg til i ønskeliste"
